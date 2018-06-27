71e6d6918200efb6fa74c1ad6243fb3b14431ed5
Hendrik Ordoñez B <hendrikob.one@gmail.com>
2018-06-26 22:09:50 -0500

    Lesson 3.6

---
+++ src/components/index.jsx
@@ -0,0 +1,21 @@
+import React, { Component, PropTypes } from 'react'
+
+class App extends Component {
+	constructor(...props){
+		super(...props)
+		this.state = {
+
+		}
+	}
+	render(){
+		return(
+			<div>Hello React!</div>
+		)
+	}
+}
+
+App.propType = {}
+
+App.defaultProps = {}
+
+export default App
